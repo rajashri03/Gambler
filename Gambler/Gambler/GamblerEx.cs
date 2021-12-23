cb7f4d8be0bc47787a0f9fae95bd1ac97f048a31
rajashri03 <telvekar.rajashri@gmail.com>
2021-12-23 10:26:39 +0530

    Add:Check betting valus is either win or loss

---
+++ Gambler/Gambler/GamblerEx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gambler
+{
+    public class GamblerEx
+    {
+        public void Display()
+        {
+            int Bet = 1;
+            int Money = 100;
+            Random bet_num = new Random();
+            int check = bet_num.Next(0, 100);
+            Console.WriteLine(check);
+            if (Bet == check)
+            {
+                Console.WriteLine("Win");
+            }
+            else
+            {
+                Console.Write("Loss");
+            }
+        }
+    }
+}
---
+++ Gambler/Gambler/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Gambler;
+GamblerEx gam = new GamblerEx();
+gam.Display();
